2a96ed9ff5bd35a7b953bb0d24079652b48f75b5
Jon Baltz <baltz.jon.r@gmail.com>
2020-03-23 21:05:54 -0600

    Setup proxy server repository

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
